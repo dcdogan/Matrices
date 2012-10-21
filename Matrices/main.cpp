35df8ccb081d3d3e2e8d2723bcf534e4c0093322
Dogan Can <ddogan1@binghamton.edu>
2012-10-22 00:19:01 +0300

    Initial Commit

---
+++ Matrices/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Matrices
+//
+//  Created by DoganCan on 10/22/12.
+//  Copyright (c) 2012 Dogan Can Dogan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
